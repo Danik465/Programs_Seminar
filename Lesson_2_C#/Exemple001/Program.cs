df48fdd373d495fe506375bef106700767847266
Danila <dan_crysis@mail.ru>
2022-06-25 13:34:49 +0300

    Вторая лекция

---
+++ Lesson_2_C#/Exemple001/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         int Max(int arg1, int arg2, int arg3)
+        {
+            int result = arg1;
+            if  (arg2 > result)result = arg2;
+            if  (arg3 > result)result = arg3;
+            return result;
+        }
+
+       int a1= 2;
+       int b1 =232;
+       int c1 = 112;
+
+       int a2 = 14;
+       int b2 =13;
+       int c2 = 23;
+
+       int a3 = 13;
+       int b3 =23;
+       int c3 = 15;
+
+       int max = a1;
+
+
+        int max1 = Max(a1, b1, c1);
+        int max2 = Max(a2, b2 ,c2);
+        int max3 = Max(a3, b3, c3);
+        int result = Max(max1, max2,max3);
+       Console.Write(result);
+       
+    }
+}
---
+++ Lesson_2_C#/Exemple003/Program.cs
@@ -0,0 +1,23 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = {1,2,33,4,18,16,17,18};
+
+        int n = array.Length;
+        int find = 18;
+
+        int index = 0;
+
+        while (index < n)
+        {
+
+            if(array[index] == find)
+            {
+                Console.WriteLine(index);
+                break;
+            }
+            index++;
+        }
+    }
+}
