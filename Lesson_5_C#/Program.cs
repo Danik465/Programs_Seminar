62a9abf71863400e591f74bf7d70ff23a8fca2cd
Danila <dan_crysis@mail.ru>
2022-07-19 23:20:40 +0300

    Домашняя работа 8 первая задача

---
+++ Lesson_5_C#/Program.cs
@@ -0,0 +1,47 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        // string Zadacha1 (int a , int b)
+        // {
+        //     string result = String.Empty;
+        //     for (int i = a; i >= b ; i--)
+        //     {
+        //         result += $"{i} ";
+        //     }
+        //     return result;
+        // }
+
+        // string Zadacha1Rec (int a , int b)
+        // {
+
+        //     if (a<= b) return Zadacha1Rec(a+1,b)+$"{a} " ;
+
+        //     else return String.Empty;
+        // }
+
+        //Console.WriteLine(Zadacha1(10,1));
+        //Console.WriteLine(Zadacha1Rec(1,10));
+        Console.WriteLine(Zadacha2(10));
+        Console.WriteLine(Zadacha2Rec(10));
+        int Zadacha2(int n);
+        {
+            int result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+        int Zadacha2Rec(int n)
+        {
+            if (n == 0) return 0;
+            else return n + Zadacha2Rec(n - 1);
+        }
+
+
+
+        
+
+    }
+}
---
+++ Sem01_HomeWork/HomeWork008/Program.cs
@@ -0,0 +1,100 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит
+        // по убыванию элементы каждой строки двумерного массива.
+        // Например, задан массив:
+        // 1 4 7 2
+        // 5 9 2 3
+        // 8 4 2 4
+        // В итоге получается вот такой массив:
+        // 1 2 4 7
+        // 2 3 5 9
+        // 2 4 4 8
+        Zadacha54();
+        void Zadacha54()
+        {
+            Random rand = new Random();
+            int rows = rand.Next(4, 6);
+            int columns = rand.Next(5, 8);
+            Console.WriteLine("Введите начальный диапазон чисел в массиве:");
+            int startNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите конечный диапазон чисел в массиве:");
+            int finishNumber = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix = new int[rows, columns];
+            Console.WriteLine("Задан массив:");
+            FillMatrix(matrix, startNumber, finishNumber);
+            PrintMatrix(matrix);
+            Console.WriteLine("Отсортированный массив");
+            SortedMatrix(matrix);
+            PrintMatrix(matrix);
+
+
+        }
+
+        int[,] FillMatrix(int[,] arr, int startNumber, int FinishNumber)
+        {
+            Random random = new Random();
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    arr[i, j] = random.Next(startNumber, FinishNumber);
+                }
+            }
+            return arr;
+        }
+
+
+        int[,] PrintMatrix(int[,] arr)
+        {
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            return arr;
+        }
+
+        int[,] SortedMatrix(int[,] arr)
+        {
+            // int temp = 0;
+
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows-1; i++)
+            {
+
+                for (int j = 0; j < columns-1; j++)
+                {
+                    // int min = arr[j, 0];
+                    for (int k = 0; k < columns-1; k++)
+                    {
+
+                        if (arr[j, k] > arr[j,k+1] )
+                        {
+                           (arr[j,k], arr[j,k+1]) = (arr[j,k+1],arr[j,k]);
+                        }
+
+                    }
+                    // temp = 0;
+                }
+
+            }
+            return arr;
+        }
+
+
+
+
+    }
+}
