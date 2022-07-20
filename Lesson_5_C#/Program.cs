2012cbcf6ce145f30018a65d8c3a57738d1045d5
Danila <dan_crysis@mail.ru>
2022-07-20 15:28:18 +0300

    Задача 56

--- Lesson_5_C#/Program.cs
+++ Lesson_5_C#/Program.cs
@@ -18,26 +18,26 @@
         //     if (a<= b) return Zadacha1Rec(a+1,b)+$"{a} " ;
 
         //     else return String.Empty;
-        // }
+        // // }
 
-        //Console.WriteLine(Zadacha1(10,1));
-        //Console.WriteLine(Zadacha1Rec(1,10));
-        Console.WriteLine(Zadacha2(10));
-        Console.WriteLine(Zadacha2Rec(10));
-        int Zadacha2(int n);
-        {
-            int result = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                result += i;
-            }
-            return result;
-        }
-        int Zadacha2Rec(int n)
-        {
-            if (n == 0) return 0;
-            else return n + Zadacha2Rec(n - 1);
-        }
+        // //Console.WriteLine(Zadacha1(10,1));
+        // //Console.WriteLine(Zadacha1Rec(1,10));
+        // Console.WriteLine(Zadacha2(10));
+        // Console.WriteLine(Zadacha2Rec(10));
+        // int Zadacha2(int n);
+        // {
+        //     int result = 0;
+        //     for (int i = 1; i <= n; i++)
+        //     {
+        //         result += i;
+        //     }
+        //     return result;
+        // }
+        // int Zadacha2Rec(int n)
+        // {
+        //     if (n == 0) return 0;
+        //     else return n + Zadacha2Rec(n - 1);
+        // }
 
 
 
--- Sem01_HomeWork/HomeWork008/Program.cs
+++ Sem01_HomeWork/HomeWork008/Program.cs
@@ -12,12 +12,14 @@
         // 1 2 4 7
         // 2 3 5 9
         // 2 4 4 8
-        Zadacha54();
+        // Zadacha54();
         void Zadacha54()
         {
             Random rand = new Random();
-            int rows = rand.Next(4, 6);
-            int columns = rand.Next(5, 8);
+            // int rows = rand.Next(4, 6);
+            // int columns = rand.Next(5, 8);
+            int rows = Convert.ToInt32(Console.ReadLine());
+            int columns = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите начальный диапазон чисел в массиве:");
             int startNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите конечный диапазон чисел в массиве:");
@@ -71,18 +73,18 @@
 
             int rows = arr.GetLength(0);
             int columns = arr.GetLength(1);
-            for (int i = 0; i < rows-1; i++)
+            for (int i = 0; i < rows; i++)
             {
 
-                for (int j = 0; j < columns-1; j++)
+                for (int j = 0; j < columns - 1; j++)
                 {
                     // int min = arr[j, 0];
-                    for (int k = 0; k < columns-1; k++)
+                    for (int k = 0; k < columns - 1; k++)
                     {
 
-                        if (arr[j, k] > arr[j,k+1] )
+                        if (arr[j, k] > arr[j, k + 1])
                         {
-                           (arr[j,k], arr[j,k+1]) = (arr[j,k+1],arr[j,k]);
+                            (arr[j, k], arr[j, k + 1]) = (arr[j, k + 1], arr[j, k]);
                         }
 
                     }
@@ -93,8 +95,80 @@
             return arr;
         }
 
+        //         Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+        //  которая будет находить строку с наименьшей суммой элементов.
+        // Например, задан массив:
+        // 1 4 7 2
+        // 5 9 2 3
+        // 8 4 2 4
+        // 5 2 6 7
+        // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей
+        //  суммой элементов: 1 строка
+        Zadacha56();
+        void Zadacha56()
+        {
+            Random random = new Random();
+            int rows = random.Next(3, 5);
+            int columns = random.Next(5, 7);
+            int[,] massive = new int[rows, columns];
+            Console.WriteLine("Введите начальный диапазон чисел в массиве:");
+            int startNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите конечный диапазон чисел в массиве:");
+            int finishNumber = Convert.ToInt32(Console.ReadLine());
 
+            FillMatrix(massive, startNumber, finishNumber);
+            PrintMatrix(massive);
+            Console.WriteLine("Двумерный массив:");
+            FillMinSum(massive);
+            Console.WriteLine("Массив из сумм строк:");
+            PrintMatrixOnes(FillMinSum(massive));
+            Console.WriteLine();
+            Console.WriteLine("Минимальная сумма строки:");
+            int min = MinSum(FillMinSum(massive));
+            Console.WriteLine(min);
+        }
+        int[] PrintMatrixOnes(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            return arr;
+        }
 
+        int[] FillMinSum(int[,] arr)
+        {
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            int[] array = new int[rows];
+            int index = 0;
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < columns; j++)
+                {
+                    sum += arr[i, j];
+
+                }
+                array[index] = sum;
+                index++;
+            }
+            return array;
+        }
+
+        int MinSum(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
 
     }
 }
