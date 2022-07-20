18843b52a1aff772bdae970d4d198d0652840e5e
Danila <dan_crysis@mail.ru>
2022-07-20 17:45:33 +0300

    Доделал дз

--- Sem01_HomeWork/HomeWork008/Program.cs
+++ Sem01_HomeWork/HomeWork008/Program.cs
@@ -104,7 +104,7 @@
         // 5 2 6 7
         // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей
         //  суммой элементов: 1 строка
-        Zadacha56();
+        // Zadacha56();
         void Zadacha56()
         {
             Random random = new Random();
@@ -115,10 +115,9 @@
             int startNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите конечный диапазон чисел в массиве:");
             int finishNumber = Convert.ToInt32(Console.ReadLine());
-
+            Console.WriteLine("Двумерный массив:");
             FillMatrix(massive, startNumber, finishNumber);
             PrintMatrix(massive);
-            Console.WriteLine("Двумерный массив:");
             FillMinSum(massive);
             Console.WriteLine("Массив из сумм строк:");
             PrintMatrixOnes(FillMinSum(massive));
@@ -169,6 +168,106 @@
             }
             return min;
         }
+
+
+
+
+        //         Задача 58. Заполните спирально массив 4 на 4.
+        // Например, на выходе получается вот такой массив:
+        // 1  2  3  4
+        // 12 13 14 5
+        // 11 16 15 6
+        // 10  9  8  7
+
+
+        Zadacha58();
+        void Zadacha58()
+        {
+            Random random = new Random();
+
+            int[,] massive = new int[4, 4];
+
+            Console.WriteLine("Двумерный массив:");
+            FillMassivSpiral(massive, 1, 16);
+            PrintMatrix(massive);
+
+        }
+        int[,] FillMassivSpiral(int[,] arr, int startNumber, int finishNumber)
+        {
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                int index1 = 1;
+                for (int j = 0; j < columns; j++)
+                {
+                    if (i == 0) arr[i, j] = index1;
+
+                    index1++;
+                }
+
+            }
+            for (int i = rows; i >= 0; i--)
+            {
+                int index2 = 6;
+                for (int j = columns; j >= 0; j--)
+                {
+                    if (j <= columns - 1 && i == rows - 1) arr[i, j] = index2;
+
+                    index2++;
+                }
+
+            }
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                int index4 = 10;
+                for (int j = columns - 1; j >= 0; j--)
+                {
+                    if (j != 0 && i == rows - 4 && j != columns - 1) arr[j, i] = index4;
+
+                    index4++;
+                }
+
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                int index3 = 4;
+                for (int j = 0; j < columns; j++)
+                {
+                    if (i == rows - 1 && j <= columns - 1) arr[j, i] = index3;
+
+                    index3++;
+                }
+
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                int index5 = 12;
+                for (int j = 0; j < columns; j++)
+                {
+                    if (i == 1 && j != 0 && j != columns - 1) arr[i, j] = index5;
+
+                    index5++;
+                }
+
+            }
+            for (int i = rows; i >= 0; i--)
+            {
+                int index6 = 14;
+                for (int j = columns; j >= 0; j--)
+                {
+                    
+                    if (j!= 0&&j <= columns - 2 && i == rows - 2) arr[i, j] = index6;
+
+                    index6++;
+                }
+
+            }
+            return arr;
+        }
+
+
 
     }
 }
